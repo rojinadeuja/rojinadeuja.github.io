7844b65daf83184d1502b6e84e3f5ed72d535f14
Rojina Deuja <rojinadeuja33g@gmail.com>
2020-09-22 15:12:52 -0500

    Updates

---
+++ precache-manifest.42bf2006c7c8ff5ddf3b2621601a9eec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf1a6c8c8293bceb3941daafdab81d8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ee37f03655dcaf7cfea",
+    "url": "/static/css/main.861cb798.chunk.css"
+  },
+  {
+    "revision": "4c283ce0442984729e13",
+    "url": "/static/js/2.7398e386.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7398e386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ee37f03655dcaf7cfea",
+    "url": "/static/js/main.fac458a6.chunk.js"
+  },
+  {
+    "revision": "6afff059487724a0cb4d",
+    "url": "/static/js/runtime-main.34d5b3d1.js"
+  }
+]);
