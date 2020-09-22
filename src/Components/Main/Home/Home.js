70125c9cd64952b46bbbbefbb9eaa9c11bf0e531
Rojina Deuja <rojinadeuja33g@gmail.com>
2020-09-22 16:19:08 -0500

    added image, changed styling

--- src/Components/Main/Home/Home.js
+++ src/Components/Main/Home/Home.js
@@ -3,22 +3,25 @@ import React from 'react';
 const home = () => {
 
     return <div>
-        <p className="connect-links">
+        <div className="connect-links">
             <a className="connect-link" target="_blank" href="https://www.linkedin.com/in/rojinadeuja/">LinkedIn</a>
             <span>{'    '}</span>
             <a className="connect-link" target="_blank" href="https://github.com/rojinadeuja">Github</a>
-        </p>
-        <p className="large-text" >
-            <p>
-                <strong>Hello! I am Rojina Deuja, a MSCS student at the University of Nebraska-Lincoln (UNL)</strong>, working as a Graduate Teaching Assistant for courses closely related to Data and Software Engineering. I have over 2 years of industry experience providing Data Warehousing and Business Intelligence services for clients in and outside the US.
+        </div>
+        <div className="profile-section">
+            <img className="profile-img" src="assets/profile.jpg"></img>
+            <div className="large-text profile-txt" >
+                <p>
+                    <strong>Hello! I am Rojina Deuja, a MSCS student at the University of Nebraska-Lincoln (UNL)</strong>, working as a Graduate Teaching Assistant for courses closely related to Data and Software Engineering. I have over 2 years of industry experience providing Data Warehousing and Business Intelligence services for clients in and outside the US.
             </p>
-            <p>
-                I am working with Dr. Mohammad Rashedul Hasan on Natural Language Processing, exploring ways to handle enterprise-level data to understand customers and improve businesses. My goal is to relay the knowledge and experiences I have gathered working with multiple businesses into creating innovative and intelligent software solutions.
+                <p>
+                    I am working with Dr. Mohammad Rashedul Hasan on Natural Language Processing, exploring ways to handle enterprise-level data to understand customers and improve businesses. My goal is to relay the knowledge and experiences I have gathered working with multiple businesses into creating innovative and intelligent software solutions.
             </p>
-            <p>
-                <strong>I have a strong programming background.</strong> I enjoy learning new things and can assimilate new ideas quickly. My ideal job would be a Full-stack Software Developer. <strong>I am currently looking for Summer 2021 internships!</strong>
-            </p>
-        </p>
+                <p>
+                    <strong>I have a strong programming background.</strong> I enjoy learning new things and can assimilate new ideas quickly. My ideal job would be a Full-stack Software Developer. <strong>I am currently looking for Summer 2021 internships!</strong>
+                </p>
+            </div>
+        </div>
     </div>
 }
 
--- src/Components/Shared/Footer.js
+++ src/Components/Shared/Footer.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
 const footer = () => {
-    return <form id='download' target="_blank" method="get" action="assets/deuja_rojina_resume_2020.pdf">
-        <button id='btn-download' type="submit">Download Resume</button>
+    return <form id='download' method="get" action="assets/deuja_rojina_resume_2020.pdf">
+        <button id='btn-download' type="submit"><span style={{ paddingBottom: '0.5em', borderBottomStyle: 'solid' }}>Download Resume</span></button>
     </form>
-    // <ul className="footer-links">
-    //     <li><a className="footer-link" href="https://github.com/rojinadeuja">Github</a></li>
-    //     <li><a className="footer-link" href="https://www.linkedin.com/in/rojinadeuja/">LinkedIn</a></li>
-    // </ul>
 }
 
 export default footer;
