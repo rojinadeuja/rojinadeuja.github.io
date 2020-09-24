1d6cc5363fc680682bcb034013fabfd2b667f504
rojinadeuja <rojinadeuja33g@gmail.com>
2020-09-23 23:07:35 -0500

    Changed css

--- src/Components/Main/Home/Home.js
+++ src/Components/Main/Home/Home.js
@@ -1,20 +1,21 @@
 import React from 'react';
 
 const home = () => {
-    return <div>
-    <aside class="profile-card">
-    <header>
-        <a href="#">
-            <img src="rose-in-window.png" />
-        </a>
-        <h1>Rojina Deuja</h1>
-        <h2>M.S. in Computer Science, University of Nebraska-Lincoln</h2>
-    </header>
-    <div class="profile-bio">
-        <p>Hello, I'm Rojina Deuja.</p>
+    return(
+    <div class="profile-card">
+        <header>
+            <h1>ROJINA DEUJA</h1>
+            <h2>M.S. in Computer Science, University of Nebraska-Lincoln</h2>
+        </header>
+        <div class="profile-bio">
+        <p>In 2019, I travelled half-way acorss the world to pursue Computer Science in the USA.</p>
+        </div>
+        <div>
+        <form id='download' method="get" action="assets/deuja_rojina_resume_2020.pdf">
+            <button id='btn-download' type="submit"><span style={{ paddingBottom: '0.5em', borderBottomStyle: 'solid' }}>Download Resume</span></button>
+        </form>
     </div>
-</aside>
-    </div>
+    </div>)
 }
 
 export default home;
