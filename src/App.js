ad915b46ced66da16a581c385f64c503cc350a02
rojinadeuja <rojinadeuja33g@gmail.com>
2020-09-24 09:46:40 -0500

    Cleaned out animations

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import './App.css';
-import Header from './Components/Shared/Header';
 import Main from './Components/Main/Main';
-import Footer from './Components/Shared/Footer';
 
 
 function App() {
   return (
     <div className="wrapper">
-      <header className="header">
-        <Header></Header>
-      </header>
       <main className="main">
         <Main></Main>
       </main>
-      <footer className="footer">
-        <Footer></Footer>
-      </footer>
     </div>
   );
 }
