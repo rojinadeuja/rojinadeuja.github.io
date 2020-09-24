2fce529a9b520947fb1a65267a932e7c01c7bf8f
rojinadeuja <rojinadeuja33g@gmail.com>
2020-09-24 16:21:15 -0500

    Changed up font

--- src/Components/Main/Home/Home.js
+++ src/Components/Main/Home/Home.js
@@ -10,7 +10,7 @@ const home = () => {
                     <h2>M.S. in Computer Science, University of Nebraska-Lincoln</h2>
                 </header>
                 <div className="profile-bio">
-                    <p>In 2019, I travelled half-way acorss the world to pursue Computer Science in the USA. So far, so great!<br/>I work as a Graduate Teaching Assitant facilitating courses related to Data Modeling and Software Engineering.<br/>I love learning about the human brain, so I do my research on Machine Learning and listen to podcasts. Invisibilia and Hidden Brain are my favorites!<br/><br/>
+                    <p><span className="large-text">In 2019,</span> I travelled half-way acorss the world to pursue Computer Science in the USA. So far, so great!<br/>I work as a Graduate Teaching Assitant facilitating courses related to Data Modeling and Software Engineering.<br/>I love learning about the human brain, so I do my research on Machine Learning and listen to podcasts. Invisibilia and Hidden Brain are my favorites!<br/><br/>
                     <h3>I'm currently seeking Sofware Development Internship opportunity for Summer 2021. </h3>Lets get in touch &rarr; <a href="mailto: rojinadeuja33g@gmail.com"> Email</a></p><br/>
                     <form id='download' method="get" action="assets/deuja_rojina_resume_2020.pdf" target="_blank" rel="noopener noreferrer">
                         <button id='btn-download' type="submit"className="btn-download"><span style={{ paddingBottom: '0.3em', borderBottomStyle: 'solid' }}>View Resume</span></button>
