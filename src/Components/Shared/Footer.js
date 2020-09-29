79e9ea7039c40e44a0ab464f8a2b6c33a06281c1
rojinadeuja <rojinadeuja33g@gmail.com>
2020-09-29 11:14:16 -0500

    Add CSS for chevron button

--- src/Components/Shared/Footer.js
+++ src/Components/Shared/Footer.js
@@ -4,6 +4,9 @@ const footer = () => {
     return (
     <div className="main-footer">
         <p>Designed &amp; Developed by Rojina Deuja | Inspired by <a href="https://codepen.io/VictoryDesign" target="_blank" rel="noopener noreferrer">David Jones</a></p>
+        <div>
+            <a href="#"><i className="fa fa-chevron-circle-down footer-chevron"></i></a>
+        </div>    
     </div>)
 }
 
