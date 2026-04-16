338f8f91f83e9a8a58e37f15c7002bdf451b9cbb
Sudeep Basnet <rosesudu@Sudeeps-MacBook-Air.local>
2026-04-16 11:47:41 -0700

    Update resume

--- src/Components/Shared/Footer.js
+++ src/Components/Shared/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const footer = () => {
     return (
     <div className="main-footer">
-        <p>Designed &amp; Developed by Rojina Deuja | Inspired by <a href="https://codepen.io/VictoryDesign" target="_blank" rel="noopener noreferrer">David Jones</a></p>
+        <p>Designed &amp; Developed by Rojina Deuja | 2021</p>
         {/* <div>
             <a href="#"><i className="fa fa-chevron-circle-down footer-chevron"></i></a>
         </div>     */}
